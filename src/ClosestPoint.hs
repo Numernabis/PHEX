b3e78875fbf6bfa5a69ab0f9e4ef10062e6999de
Wojtek Wanczyk <wojtekwanczyk@gmail.com>
2018-01-25 10:28:46 +0100

    edited ClosestPoint descriptiopn

--- src/ClosestPoint.hs
+++ src/ClosestPoint.hs
@@ -1,15 +1,8 @@
 {-|
 Module      : ClosestPoint
-Description : Short description
-Copyright   : (c) Some Guy, 2013
-                  Someone Else, 2014
-License     : GPL-3
-Maintainer  : sample@email.com
-Stability   : experimental
-Portability : POSIX
+Maintainer  : wojtekwanczyk@gmail.com
 
-Here is a longer description of this module, containing some
-commentary with @some markup@.
+
 -}
 module ClosestPoint where
 
